99d86c3382c301f171c8ab6ebadfed68a4eb7eb5
Javiiii20 <ordoezjavier23@gmail.com>
2024-05-10 20:12:59 -0600

    Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import { Footer } from './components/Footer';
 import MediaCard from './components/MediaCard';
@@ -27,9 +27,9 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
-          <Route path="/admin" element={isLoggedIn ? <AdminPage onLogout={handleLogout} /> : <Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+          <Route path="https://clinicarenacergt.onrender.com/" element={<HomePage isLoggedIn={isLoggedIn} />} />
+          <Route path="https://clinicarenacergt.onrender.com/admin" element={isLoggedIn ? <AdminPage onLogout={handleLogout} /> : <Navigate to="https://clinicarenacergt.onrender.com/login" />} />
+          <Route path="https://clinicarenacergt.onrender.com/login" element={<LoginPage onLogin={handleLogin} />} />
         </Routes>
         <Footer />
       </div>
